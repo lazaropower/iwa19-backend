bce5e5aa063e59f0502216485f061fa3115fb383
Lázaro <lazaroesunagranpersona@gmail.com>
2019-06-17 11:20:52 +0200

    Added the first base of the backend and hello world

---
+++ src/main/java/com/lazaropower/iwa/backend/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.lazaropower.iwa.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){ return "hello"; }
+}
